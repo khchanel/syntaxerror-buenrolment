6edac227ea31b2f0a65193b2f0379dba50b28f33
Nelson Chan <khchanel@gmail.com>
2013-05-18 15:45:08 +1000

    add System.Web.Optimization package

---
+++ BUEnrolment/App_Start/BundleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web;
+using System.Web.Optimization;
+
+namespace BUEnrolment.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+        }
+    }
+}
