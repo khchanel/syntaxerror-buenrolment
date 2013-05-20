fc2d8ba2238b3025199a4762200c34fffc3f2dc2
Justin Favaloro <justin@justin.com>
2013-05-21 05:28:08 +1000

    Created the script to move items back and fourth between the two lists

--- BUEnrolment/Content/js/script.js
+++ BUEnrolment/Content/js/script.js
@@ -1,1 +1,24 @@
-﻿
+﻿function moveItems(sourceID, destID) {
+    var src = document.getElementById(sourceID);
+    var dest = document.getElementById(destID);
+
+    for (var count = 0; count < src.options.length; count++) {
+
+        if (src.options[count].selected == true) {
+            var option = src.options[count];
+
+            var newOption = document.createElement("option");
+            newOption.value = option.value;
+            newOption.text = option.text;
+            newOption.selected = true;
+            try {
+                dest.add(newOption, null); //Standard
+                src.remove(count, null);
+            } catch (error) {
+                dest.add(newOption); // IE only
+                src.remove(count);
+            }
+            count--;
+        }
+    }
+}
