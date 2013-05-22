3a42b2a9948bf2b93208a98bc3b1bdb8a299f330
Nelson Chan <khchanel@gmail.com>
2013-05-23 00:30:58 +1000

    change user password min length from 6 to 2

--- BUEnrolment/Models/AccountModels.cs
+++ BUEnrolment/Models/AccountModels.cs
@@ -47,7 +47,7 @@ namespace BUEnrolment.Models
         public string FullName { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
