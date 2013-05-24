0dc8351e14efedb05eeeb6cdbd6035deb85c5925
Nelson Chan <khchanel@gmail.com>
2013-05-25 00:22:10 +1000

    Create Result controller and default actions

---
+++ BUEnrolment/Controllers/ResultController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BUEnrolment.Models;
+
+namespace BUEnrolment.Controllers
+{
+    public class ResultController : Controller
+    {
+        private BUEnrolmentContext db = new BUEnrolmentContext();
+
+        //
+        // GET: /Result/
+
+        public ActionResult Index()
+        {
+            return View(db.Results.ToList());
+        }
+
+        //
+        // GET: /Result/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Result result = db.Results.Find(id);
+            if (result == null)
+            {
+                return HttpNotFound();
+            }
+            return View(result);
+        }
+
+        //
+        // GET: /Result/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Result/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Result result)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Results.Add(result);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(result);
+        }
+
+        //
+        // GET: /Result/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Result result = db.Results.Find(id);
+            if (result == null)
+            {
+                return HttpNotFound();
+            }
+            return View(result);
+        }
+
+        //
+        // POST: /Result/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Result result)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(result).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(result);
+        }
+
+        //
+        // GET: /Result/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Result result = db.Results.Find(id);
+            if (result == null)
+            {
+                return HttpNotFound();
+            }
+            return View(result);
+        }
+
+        //
+        // POST: /Result/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Result result = db.Results.Find(id);
+            db.Results.Remove(result);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
