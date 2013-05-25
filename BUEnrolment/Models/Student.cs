8c31e4cd77c89ccc9ac6c2142a10b9bb59d61a2a
Jason King <jason.king1311@gmail.com>
2013-05-25 16:22:09 +1000

    Validation for enrolment
    Added validation when a student tries to list the possible enrolment
    subjects.
    Applies the following filters:
    1. Currently enrolled subjects
    2. Subjects already passed
    3. Subjects where the max enrolment is reached
    4. Subjects that have been failed 3 times
    5. Subjects where the prerequisites have not yet been completed

--- BUEnrolment/Models/Student.cs
+++ BUEnrolment/Models/Student.cs
@@ -18,12 +18,14 @@ namespace BUEnrolment.Models
 
         public virtual List<Subject> EnrolledSubjects { get; set; }
         public virtual List<Request> Requests { get; set; }
-        public IDictionary<Subject, Result> CompletedSubjects { get; set; }
+        public virtual List<Tuple<Subject, Result>> CompletedSubjects { get; set; }
 
         public Student()
         {
-            this.Requests = new List<Request>();
-            this.EnrolledSubjects = new List<Subject>();
+            Requests = new List<Request>();
+            EnrolledSubjects = new List<Subject>();
+            CompletedSubjects = new List<Tuple<Subject, Result>>();
+
         }
 
         public void EnrolSubject(Subject subject)
@@ -31,10 +33,70 @@ namespace BUEnrolment.Models
             EnrolledSubjects.Add(subject);
         }
 
-        public List<Subject> GetRequestableSubjects(List<Subject> allSubjects)
+        public void CompleteSubject(Subject subject, Result result)
         {
+            CompletedSubjects.Add(Tuple.Create(subject, result));
+        }
+
+        public List<Subject> GetEnrollableSubjects(List<Subject> allSubjects)
+        {
+            RemoveCurrentlyEnrolled(ref allSubjects);
+            RemovePassed(ref allSubjects);
+            RemoveMaxEnrolmentReached(ref allSubjects);
+            RemoveFailedThreeTimes(ref allSubjects);
+            RemovePrerequisitesNotCompleted(ref allSubjects);
+
             return allSubjects;
         }
 
+        public void RemoveCurrentlyEnrolled(ref List<Subject> enrollableSubjects)
+        {
+            enrollableSubjects = enrollableSubjects.Except(EnrolledSubjects).ToList();
+        }
+
+        public void RemovePassed(ref List<Subject> enrollableSubjects)
+        {
+            foreach (Tuple<Subject, Result> completedSubject in CompletedSubjects.Where(completedSubject => completedSubject.Item2.Mark > 49))
+            {
+                enrollableSubjects.Remove(completedSubject.Item1);
+            }   
+        }
+
+        public void RemoveMaxEnrolmentReached(ref List<Subject> enrollableSubjects)
+        {
+            foreach (Subject subject in enrollableSubjects.Where(subject => subject.MaxEnrolmentIsReached()))
+            {
+                enrollableSubjects.Remove(subject);
+            }
+        }
+
+        public void RemoveFailedThreeTimes(ref List<Subject> enrollableSubjects)
+        {
+            foreach (Subject subject in enrollableSubjects.Where(subject => CompletedSubjects.Count(s => s.Item1 == subject && s.Item2.Mark < 50) == 3))
+            {
+                enrollableSubjects.Remove(subject);
+            }
+        }
+
+        public void RemovePrerequisitesNotCompleted(ref List<Subject> enrollableSubjects)
+        {
+            foreach (Subject subject in enrollableSubjects.Where(subject => !subject.Prerequisites.Except(CompletedSubjects.Where(m => m.Item2.Mark >= 50).Select(s => s.Item1)).Any()))
+            {
+                enrollableSubjects.Remove(subject);
+            }
+        }
+
+        public List<Subject> GetRequestableSubjects(List<Subject> allSubjects)
+        {
+            RemoveCurrentlyEnrolled(ref allSubjects);
+            RemovePassed(ref allSubjects);
+
+            return allSubjects;
+        }
+
+        public bool FullyEnrolled()
+        {
+            return EnrolledSubjects.Count >= 4;
+        }
     }
 }
