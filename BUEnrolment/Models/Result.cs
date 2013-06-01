1c822e2af689bcc8ed740d10a36e2c152bb0e17d
Nelson Chan <khchanel@gmail.com>
2013-06-01 14:20:27 +1000

    Fall back to simple Result Grade

--- BUEnrolment/Models/Result.cs
+++ BUEnrolment/Models/Result.cs
@@ -37,45 +37,33 @@ namespace BUEnrolment.Models
             }
         }
         public ResultGrade Grade { get; private set; }
-        public Dictionary<ResultGrade, double> GradeMap { get; private set; }
 
 
         public Result()
         {
-            SetupGradeMap();
+            Grade = ResultGrade.Fail;
         }
 
 
-        private void SetupGradeMap()
-        {
-            NameValueCollection appSettings = ConfigurationManager.AppSettings;
-
-            GradeMap = new Dictionary<ResultGrade, double>();
-            GradeMap.Add(ResultGrade.HighDistinction, Convert.ToInt32(appSettings["HighDistinction"]));
-            GradeMap.Add(ResultGrade.Distinction, Convert.ToInt32(appSettings["Distinction"]));
-            GradeMap.Add(ResultGrade.Credit, Convert.ToInt32(appSettings["Credit"]));
-            GradeMap.Add(ResultGrade.Pass, Convert.ToInt32(appSettings["Pass"]));
-            GradeMap.Add(ResultGrade.Fail, Convert.ToInt32(appSettings["Fail"]));
-
-        }
 
         private ResultGrade MarkToGrade(double mark)
         {
+            NameValueCollection appSettings = ConfigurationManager.AppSettings;
             ResultGrade grade;
 
-            if (mark >= GradeMap[ResultGrade.HighDistinction])
+            if (mark >= Convert.ToInt32(appSettings["HighDistinction"]))
             {
                 grade = ResultGrade.HighDistinction;
             }
-            else if (mark >= GradeMap[ResultGrade.Distinction])
+            else if (mark >= Convert.ToInt32(appSettings["Distinction"]))
             {
                 grade = ResultGrade.Distinction;
             }
-            else if (mark >= GradeMap[ResultGrade.Credit])
+            else if (mark >= Convert.ToInt32(appSettings["Credit"]))
             {
                 grade = ResultGrade.Credit;
             }
-            else if (mark >= GradeMap[ResultGrade.Pass])
+            else if (mark >= Convert.ToInt32(appSettings["Pass"]))
             {
                 grade = ResultGrade.Pass;
             }
