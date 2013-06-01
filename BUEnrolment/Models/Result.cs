7db81e93658ada0c19e636dc4f9d501e83968744
Nelson Chan <khchanel@gmail.com>
2013-06-01 14:16:02 +1000

    Implement Result Grade

--- BUEnrolment/Models/Result.cs
+++ BUEnrolment/Models/Result.cs
@@ -3,40 +3,88 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Configuration;
+using System.Collections.Specialized;
 
 namespace BUEnrolment.Models
 {
     public class Result
     {
+
+        public enum ResultGrade
+        {
+            HighDistinction,
+            Distinction,
+            Credit,
+            Pass,
+            Fail
+        }
+
+        private double mark;
+
         [Key]
         public int Id { get; set; }
-        public double Mark { get; set; }
         public Subject subject { get; set; }
-        public string Grade
-        {
+        public double Mark {
             get
             {
-                return null;
+                return mark;
             }
             set
             {
-                ;
-                //Grade = "Fail";
-                //convert Mark to Grade
-               // throw new NotImplementedException();
+                mark = value;
+                Grade = MarkToGrade(mark);
             }
-
         }
+        public ResultGrade Grade { get; private set; }
+        public Dictionary<ResultGrade, double> GradeMap { get; private set; }
+
 
         public Result()
         {
+            SetupGradeMap();
+        }
+
+
+        private void SetupGradeMap()
+        {
+            NameValueCollection appSettings = ConfigurationManager.AppSettings;
+
+            GradeMap = new Dictionary<ResultGrade, double>();
+            GradeMap.Add(ResultGrade.HighDistinction, Convert.ToInt32(appSettings["HighDistinction"]));
+            GradeMap.Add(ResultGrade.Distinction, Convert.ToInt32(appSettings["Distinction"]));
+            GradeMap.Add(ResultGrade.Credit, Convert.ToInt32(appSettings["Credit"]));
+            GradeMap.Add(ResultGrade.Pass, Convert.ToInt32(appSettings["Pass"]));
+            GradeMap.Add(ResultGrade.Fail, Convert.ToInt32(appSettings["Fail"]));
 
         }
 
-        public Result(int mark)
+        private ResultGrade MarkToGrade(double mark)
         {
-            Mark = mark;
-            //Set Grade
+            ResultGrade grade;
+
+            if (mark >= GradeMap[ResultGrade.HighDistinction])
+            {
+                grade = ResultGrade.HighDistinction;
+            }
+            else if (mark >= GradeMap[ResultGrade.Distinction])
+            {
+                grade = ResultGrade.Distinction;
+            }
+            else if (mark >= GradeMap[ResultGrade.Credit])
+            {
+                grade = ResultGrade.Credit;
+            }
+            else if (mark >= GradeMap[ResultGrade.Pass])
+            {
+                grade = ResultGrade.Pass;
+            }
+            else
+            {
+                grade = ResultGrade.Fail;
+            }
+
+            return grade;
         }
     }
 }
