1315502d0773cc6510827030757215c3342ad934
Nelson Chan <khchanel@gmail.com>
2013-06-02 17:09:23 +1000

    add comment to Subject model

--- BUEnrolment/Models/Subject.cs
+++ BUEnrolment/Models/Subject.cs
@@ -8,31 +8,75 @@ using System.Data.Entity.ModelConfiguration;
 
 namespace BUEnrolment.Models
 {
+    /// <summary>
+    /// Subject model
+    /// </summary>
     public class Subject
     {
+        /// <summary>
+        /// Unique ID for database
+        /// </summary>
         [Key]
         public int Id { get; set; }
+
+        /// <summary>
+        /// Subject number
+        /// </summary>
         [Display(Name = "Number")]
         public string SubjectNumber { get; set; }
+
+        /// <summary>
+        /// Subject name
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Subject description
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Maximum enrolment limit for the subject
+        /// </summary>
         [Display(Name = "Max Enrolment")]
         public int MaxEnrolment { get; set; }
+
+        /// <summary>
+        /// Active status of the subject
+        /// </summary>
         public bool Active { get; set; }
 
+        /// <summary>
+        /// List of subject prerequisites
+        /// </summary>
         public virtual List<Subject> Prerequisites { get; set; }
+
+        /// <summary>
+        /// List of Student enrolled to the subject
+        /// </summary>
         [Display(Name = "Enrolled Students")]
         public virtual List<Student> EnrolledStudents { get; set; }
 
+        /// <summary>
+        /// Timestamp for concurrency purpose
+        /// </summary>
         [Timestamp]
         public Byte[] Timestamp { get; set; }
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public Subject()
         {
             this.Prerequisites = new List<Subject>();
             this.EnrolledStudents = new List<Student>();
         }
 
+
+        /// <summary>
+        /// Check if max enrolment is reached
+        /// </summary>
+        /// <returns>true if the subject is full</returns>
         public bool MaxEnrolmentIsReached()
         {
             return EnrolledStudents.Count >= MaxEnrolment;
