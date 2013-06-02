da81352942fc2f077aba404d33b10e0456103b6e
Nelson Chan <khchanel@gmail.com>
2013-06-02 15:56:46 +1000

    add comment for AccountModels

--- BUEnrolment/Models/AccountModels.cs
+++ BUEnrolment/Models/AccountModels.cs
@@ -9,24 +9,41 @@ using System.Web.Security;
 
 namespace BUEnrolment.Models
 {
-
+    /// <summary>
+    /// Login authentication model
+    /// </summary>
     public class LoginModel
     {
+        /// <summary>
+        /// User name
+        /// </summary>
         [Required]
         [Display(Name = "User name")]
         public string UserName { get; set; }
 
+        /// <summary>
+        /// User password
+        /// </summary>
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
+        /// <summary>
+        /// boolean flag for Remember me option
+        /// </summary>
         [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
     }
 
+    /// <summary>
+    /// Register model for authentication
+    /// </summary>
     public class RegisterModel
     {
+        /// <summary>
+        /// Store all user roles
+        /// </summary>
         public IEnumerable<SelectListItem> items
         {
             get
@@ -38,25 +55,40 @@ namespace BUEnrolment.Models
             }
         }
 
+        /// <summary>
+        /// User name
+        /// </summary>
         [Required]
         [Display(Name = "User name")]
         public string UserName { get; set; }
 
+        /// <summary>
+        /// User full name
+        /// </summary>
         [Required]
         [Display(Name = "Full Name")]
         public string FullName { get; set; }
 
+        /// <summary>
+        /// User password
+        /// </summary>
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
+        /// <summary>
+        /// Confirm password
+        /// </summary>
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
+        /// <summary>
+        /// User role
+        /// </summary>
         [Display(Name = "Role")]
         public string Role { get; set; }
     }
