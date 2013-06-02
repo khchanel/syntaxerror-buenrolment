81553710338e72efa9845a985dd168c740e0cd37
Nelson Chan <khchanel@gmail.com>
2013-06-02 17:00:42 +1000

    add comment to Request model

--- BUEnrolment/Models/Request.cs
+++ BUEnrolment/Models/Request.cs
@@ -6,18 +6,41 @@ using System.Web;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration;
 using System.Web.Mvc;
+
 namespace BUEnrolment.Models
 {
+    /// <summary>
+    /// Request model
+    /// </summary>
     public class Request
     {
+        /// <summary>
+        /// Unique ID for database
+        /// </summary>
         [Key]
         public int Id { get; set; }
+
+        /// <summary>
+        /// Request Reason description
+        /// </summary>
         [Required]
         [Display(Name = "Reason For Enrolment")]
         public string Description { get; set; }
+
+        /// <summary>
+        /// Reference to Subject
+        /// </summary>
         public virtual Subject Subject { get; set; }
+
+        /// <summary>
+        /// Request Status
+        /// </summary>
         public string Status { get; set; }
 
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public Request()
         {
             this.Subject = new Subject();
